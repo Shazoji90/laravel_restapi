14d4e926ea61e1fc41e57e8988308292a6b25d0e
Shazoji90 <squash7@live.com>
2023-09-06 07:24:18 +0700

    atribut aksesor stored_at

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,10 +10,10 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
-    protected $hidden = ['created_at', 'updated_at'];
-    protected $appends = ['stored_at'];
+    protected $hidden = ['created_at', 'updated_at']; /* hidden field created_at & updated_at */
+    protected $appends = ['stored_at']; /* menambah attribute yg tidak ada di dalam table/alias  */
 
-    public function getStoredAtAttribute()
+    public function getStoredAtAttribute() /*  get{NamaAtribut}Attribute, Laravel akan menganggap metode tersebut sebagai accessor untuk atribut dengan nama tersebut. */
     {
         return $this->created_at->diffForHumans();
     }
